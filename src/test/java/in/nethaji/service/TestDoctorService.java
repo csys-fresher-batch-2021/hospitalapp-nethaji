aa76feab63ce7f7857b48a9991fc5090cfcfa10a
Nethaji P <nethajirockzs@gmail.com>
2021-05-19 16:38:29 +0530

    test case issue fixed

--- src/test/java/in/nethaji/service/TestDoctorService.java
+++ src/test/java/in/nethaji/service/TestDoctorService.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import in.nethaji.model.Doctor;
